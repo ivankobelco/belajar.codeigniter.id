68326086d5a0353afa1bf8456854c82f013a2b36
Darsono <darsono72@gmail.com>
2020-07-26 07:32:16 +0700

    upload

--- application/config/database.php
+++ application/config/database.php
@@ -76,9 +76,9 @@ $query_builder = TRUE;
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'belajar',
+	'username' => 'codeigni_belajar',
+	'password' => 'lpSs4H[!66',
+	'database' => 'codeigni_belajar',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
@@ -94,3 +94,4 @@ $db['default'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
+
